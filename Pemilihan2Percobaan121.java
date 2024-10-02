420d79b5edd7e93ff9f253018ac410f7766ecc58
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2024-10-02 13:45:23 +0700

    perubahan 3

--- Pemilihan2Percobaan121.java
+++ Pemilihan2Percobaan121.java
@@ -6,12 +6,12 @@ public class Pemilihan2Percobaan121 {
         System.out.print("Masukkan tahun: ");
         int tahun = input21.nextInt();
 
-       if ((tahun % 4) == 0) {
-        if ((tahun % 100) != 0)
-            System.out.println("Tahun Kabisat");         
-       else 
+       if ((tahun % 4) == 0) 
+        if (tahun % 100 !=0) {
+            System.out.println("Tahun Kabisat");
+        } else if (tahun % 400 == 0) {
+            System.out.println("Tahun Kabisat");
+        } else 
             System.out.println("Bukan Tahun Kabisat");
-       }
-
     }
 }
