1dbc41c2913fedfe5b846a533c7bcc67eae7b2d2
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2024-10-04 17:32:20 +0700

    coba1

---
+++ Flowchart6.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Flowchart6 {
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner(System.in);
+
+        String jenisBuku;
+        int jumlahBuku;
+        final int harga = 20000;
+        double hargaTotal, diskon = 0.0, totalDiskon, hargaBayar;
+
+        System.out.println("-----------------------");
+        System.out.println("==== TOKO BUKU PASTA ====");
+        System.out.println("-----------------------");
+
+        System.out.print("Masukkan Jenis Buku (kamus/novel/buku lain): ");
+        jenisBuku = input21.nextLine();
+        System.out.print("Masukkan Jumlah Buku: ");
+        jumlahBuku = input21.nextInt();
+
+        if (jenisBuku.equalsIgnoreCase("kamus")) {
+            if (jumlahBuku > 2) {
+                diskon = 0.12;
+            } else {
+                diskon = 0.10;
+            }
+        } else if (jenisBuku.equalsIgnoreCase("novel")) {
+            if (jumlahBuku > 3) {
+                diskon = 0.09;
+            } else {
+                diskon = 0.08;
+            }
+        } else if (jenisBuku.equalsIgnoreCase("buku lain")) {
+            if (jumlahBuku > 5) {
+                diskon = 0.05; 
+            }
+        }
+
+        hargaTotal = harga * jumlahBuku;
+        totalDiskon = hargaTotal * diskon;
+        hargaBayar = hargaTotal - totalDiskon;
+
+        System.out.println("Total harga sebelum diskon: " + hargaTotal);
+        System.out.println("Diskon yang didapat: " + totalDiskon);
+        System.out.println("Total harga yang harus dibayar: " + hargaBayar);
+    }
+}
