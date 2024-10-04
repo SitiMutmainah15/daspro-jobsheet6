54dd6e2f3bb0490432b5f0923aba4d0fa9854e35
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2024-10-04 15:53:58 +0700

    TUGAS 1

--- FlowchartMinggu6.java
+++ FlowchartMinggu6.java
@@ -6,7 +6,11 @@ public class FlowchartMinggu6 {
 
         String jenisBuku;
         int jumlahBuku, harga=20000;
-        double hargaTotal, diskon, totalDiskon;
+        double hargaTotal, diskon, totalDiskon, hargaBayar;
+
+        System.out.println("-----------------------");
+        System.out.println("==== TOKO BUKU PASTA ====");
+        System.out.println("-----------------------");
 
         System.out.print("Masukkan Jenis Buku : ");
         jenisBuku = input21.nextLine();
@@ -14,10 +18,25 @@ public class FlowchartMinggu6 {
         jumlahBuku = input21.nextInt();
 
         if (jenisBuku.equalsIgnoreCase("kamus")) {
-            diskon += 0.10;
-           }   if (jumlahBuku > 2) {
-                diskon += 0.02; 
-    
+            diskon = 0.1;
+           }if (jumlahBuku > 2) {
+                diskon = 0.12; 
+
+           } else if (jenisBuku.equalsIgnoreCase("novel")) {
+            diskon = 0.08;
+            }if (jumlahBuku > 3) {
+                        diskon = 0.09; 
+
+            } if (jenisBuku.equalsIgnoreCase("buku lain")) {
+            } if (jumlahBuku > 5) {
+                diskon = 0.05;
+
+            hargaTotal = harga * jumlahBuku;
+            totalDiskon = hargaTotal * diskon;
+            hargaBayar = hargaTotal - totalDiskon;
+
+            System.out.println("Anda mendapat diskon sebesar : " + totalDiskon);
+            System.out.println("Anda membayar buku sebesar : " + hargaBayar);
         }
     }
 
